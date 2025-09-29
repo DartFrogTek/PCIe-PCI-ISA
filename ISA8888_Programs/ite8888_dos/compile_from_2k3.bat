6c6cd9cafe0de37f9eee4a8a64906b9ac2e0a0f0
Robert Valentine <paraboliclabs@gmail.com>
2025-09-29 04:00:09 -0400

    Update compile_from_2k3.bat

--- ISA8888_Programs/ite8888_dos/compile_from_2k3.bat
+++ ISA8888_Programs/ite8888_dos/compile_from_2k3.bat
@@ -1,60 +1,29 @@
 @ECHO OFF
-SETLOCAL ENABLEEXTENSIONS
+SETLOCAL
 
-REM Clear environment
-SET PATH=C:\WINDOWS\SYSTEM32;C:\WINDOWS;C:\WINDOWS\COMMAND
-SET INCLUDE=
-SET LIB=
-
-REM Set OpenWatcom paths
 SET WATCOM=C:\WATCOM
-SET PATH=%WATCOM%\BINW;%PATH%
+SET PATH=%WATCOM%\BINW;C:\WINDOWS\SYSTEM32;C:\WINDOWS
 SET INCLUDE=%WATCOM%\H
 SET LIB=%WATCOM%\LIB286;%WATCOM%\LIB286\DOS
-SET EDPATH=%WATCOM%\EDDAT
-SET WIPFC=%WATCOM%\WIPFC
 
-ECHO OpenWatcom Environment:
-ECHO WATCOM=%WATCOM%
-ECHO LIB=%LIB%
-ECHO.
-
-REM Change to the directory containing the source
 CD /D "%~dp0"
 
-ECHO Compiling ITE8888.C...
-WCC -bt=dos -ml -ox -we -d2 ITE8888.C
-IF NOT EXIST ITE8888.OBJ (
-    ECHO Compilation failed - no object file created
-    PAUSE
-    GOTO :EOF
-)
-ECHO Compilation successful
+ECHO Compiling for 16-bit DOS...
+WCC -bt=dos -ml -ox ITE8888.C
+IF NOT EXIST ITE8888.OBJ GOTO FAIL
 
-ECHO.
 ECHO Linking...
-WLINK system dos ^
-      libpath %WATCOM%\LIB286 ^
-      libpath %WATCOM%\LIB286\DOS ^
-      library clibl ^
-      file ITE8888.OBJ ^
-      name ITE8888CFG.EXE
+WLINK system dos file ITE8888.OBJ library clibl name ITE8888.EXE
+IF NOT EXIST ITE8888.EXE GOTO FAIL
 
-IF NOT EXIST ITE8888CFG.EXE (
-    ECHO Linking failed - no executable created
-    ECHO.
-    ECHO Try running with verbose output:
-    ECHO WLINK system dos op verbose file ITE8888.OBJ name ITE8888CFG.EXE
-    PAUSE
-    GOTO :EOF
-)
+ECHO Success!
+DIR ITE8888.EXE
+DEL ITE8888.OBJ
+GOTO END
 
-ECHO.
-ECHO Build successful!
-DIR ITE8888CFG.EXE
-
-REM Cleanup
-IF EXIST ITE8888.OBJ DEL ITE8888.OBJ
-
-ENDLOCAL
+:FAIL
+ECHO Build failed
 PAUSE
+
+:END
+ENDLOCAL
